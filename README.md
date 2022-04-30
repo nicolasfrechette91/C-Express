902096e7cf2d4dacbabf083b3ef95b1ce6499d66
nicolasfrechette <nicolasfrechette91@gmail.com>
2022-04-30 19:45:33 -0400

    initial main page header / footer setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## Bienvenidos a C-Express
+
+Click the link to get started : <a href="https://nicolasfrechette91.github.io/C-Express/home.html">here</a>
---
+++ shared/headerFooter.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("body").prepend("<div class='headerBanner'>C-express</div>");
+    $("body").append("<div class='footerBanner'>C-express no esta dise&ntilde;ado para reemplazar las consultas m&eacute;dicas en persona o consultas de emergencia. En caso de emergencia, por favor acude al hospital.<br>C-express 2022</div>");
+});
